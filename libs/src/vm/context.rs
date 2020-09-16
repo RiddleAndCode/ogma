70e8e5ee215e823c8b1143becccb3722654e0ba1
Julian Popescu <jpopesculian@gmail.com>
2020-09-16 16:00:50 +0200

    Added some extra functions to manage global variables in VM Context

--- libs/src/vm/context.rs
+++ libs/src/vm/context.rs
@@ -24,6 +24,21 @@ impl Context {
         self.globals.insert(key.to_string(), Box::new(value));
     }
 
+    /// This does the same thing as `set_global` but attempts to return the variable which was replaced
+    pub fn replace_global<K: ToString, V: Any, R: Any>(
+        &mut self,
+        key: K,
+        value: V,
+    ) -> Result<Option<Box<R>>, Trap> {
+        match self.globals.insert(key.to_string(), Box::new(value)) {
+            None => Ok(None),
+            Some(val) => Ok(Some(
+                val.downcast()
+                    .map_err(|_| Trap::DowncastError(type_name::<R>()))?,
+            )),
+        }
+    }
+
     /// Get a global variable
     pub fn get_global<K: AsRef<str>, V: Any>(&self, key: K) -> Result<Option<&V>, Trap> {
         match self.globals.get(key.as_ref()) {
@@ -48,6 +63,22 @@ impl Context {
             },
         }
     }
+
+    /// Delete a global variable
+    pub fn delete_global<K: AsRef<str>>(&mut self, key: K) {
+        self.globals.remove(key.as_ref());
+    }
+
+    /// Does the same thing as `delete_global` but attempts to return the removed variable
+    pub fn remove_global<K: AsRef<str>, R: Any>(&mut self, key: K) -> Result<Option<Box<R>>, Trap> {
+        match self.globals.remove(key.as_ref()) {
+            None => Ok(None),
+            Some(val) => Ok(Some(
+                val.downcast()
+                    .map_err(|_| Trap::DowncastError(type_name::<R>()))?,
+            )),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -55,9 +86,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn globals() {
+    fn get_global() -> Result<(), Trap> {
         let mut ctx = Context::new();
         ctx.set_global::<_, u32>("hello", 1);
-        assert_eq!(ctx.get_global::<_, u32>("hello").unwrap(), Some(&1))
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, Some(&1));
+        assert_eq!(ctx.get_global::<_, u32>("cool")?, None);
+        Ok(())
+    }
+
+    #[test]
+    fn get_global_mut() -> Result<(), Trap> {
+        let mut ctx = Context::new();
+        ctx.set_global::<_, u32>("hello", 1);
+        let num = ctx.get_global_mut::<_, u32>("hello")?.unwrap();
+        *num = 2;
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, Some(&2));
+        Ok(())
+    }
+
+    #[test]
+    fn delete_global() -> Result<(), Trap> {
+        let mut ctx = Context::new();
+        ctx.set_global::<_, u32>("hello", 1);
+        ctx.delete_global("hello");
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, None);
+        Ok(())
+    }
+
+    #[test]
+    fn remove_global() -> Result<(), Trap> {
+        let mut ctx = Context::new();
+        ctx.set_global::<_, u32>("hello", 1);
+        assert_eq!(ctx.remove_global::<_, u32>("hello")?, Some(Box::new(1)));
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, None);
+        assert_eq!(ctx.remove_global::<_, u32>("hello")?, None);
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, None);
+        Ok(())
+    }
+
+    #[test]
+    fn replace_global() -> Result<(), Trap> {
+        let mut ctx = Context::new();
+        assert_eq!(ctx.replace_global::<_, u32, u32>("hello", 1)?, None);
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, Some(&1));
+        assert_eq!(
+            ctx.replace_global::<_, u32, u32>("hello", 2)?,
+            Some(Box::new(1))
+        );
+        assert_eq!(ctx.get_global::<_, u32>("hello")?, Some(&2));
+        Ok(())
     }
 }
